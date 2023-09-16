af21ab7cae801a919367f011da3465ac146305c2
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-09-16 22:06:48 +0200

    Time: 0 ms (100%), Space: 40.4 MB (83.19%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,5 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        
+        List<Integer> result = new ArrayList<>();
+        int xSize = matrix.length;
+        int ySize = matrix[0].length;
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int columnBegin = 0;
+        int columnEnd = matrix[0].length - 1;
+        
+        while(result.size() < xSize * ySize) {
+            // go right
+            for(int i = columnBegin; i <= columnEnd && result.size() < xSize * ySize; i++)
+                result.add(matrix[rowBegin][i]);
+            rowBegin++;     // increment, so this row will NOT be visited again
+            
+            // go down
+            for(int i = rowBegin; i <= rowEnd && result.size() < xSize * ySize; i++)
+                result.add(matrix[i][columnEnd]);
+            columnEnd--;    // decrement, so this column will NOT be visited again
+            
+            // go left
+            for(int i = columnEnd; i >= columnBegin && result.size() < xSize * ySize; i--)
+                result.add(matrix[rowEnd][i]);
+            rowEnd--;       // decrement, so this row will NOT be visited again
+            
+            // go up
+            for(int i = rowEnd; i >= rowBegin && result.size() < xSize * ySize; i--)
+                result.add(matrix[i][columnBegin]);
+            columnBegin++;  // increment, so this column will NOT be visited again
+        }
+        
+        return result;
+    }
+
+    public List<Integer> spiralOrder_alt(int[][] matrix) {
         // go right: increment j
         // go down: increment i
         // go left: decrement j
