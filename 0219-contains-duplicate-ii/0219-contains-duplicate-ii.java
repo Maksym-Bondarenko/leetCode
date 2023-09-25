4c592a4ad90d5d568e4cfa7ee11c92d4aaff354c
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-09-25 13:31:30 +0200

    Time: 23 ms (18.05%), Space: 55.1 MB (85.1%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,25 +1,41 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Idea 1: (HashMap): when iterating through the array remember in HM value and its position.
-        // For each number check if this value is in HM and if abs(i-j) <= k.
-        // If value is not in HM -> add it there. If constraint with k does not work -> update value and its position in HM with current one.
+        // // Idea 1: (HashMap): when iterating through the array remember in HM value and its position.
+        // // For each number check if this value is in HM and if abs(i-j) <= k.
+        // // If value is not in HM -> add it there. If constraint with k does not work -> update value and its position in HM with current one.
 
-        Map<Integer, Integer> occurrences = new HashMap<>();
+        // Map<Integer, Integer> occurrences = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++) {
-            if(occurrences.containsKey(nums[i])) {
-                // check k constraint
-                if(i - occurrences.get(nums[i]) <= k)
-                    return true;
-            }
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(occurrences.containsKey(nums[i])) {
+        //         // check k constraint
+        //         if(i - occurrences.get(nums[i]) <= k)
+        //             return true;
+        //     }
 
-            occurrences.put(nums[i], i);
-        }
+        //     occurrences.put(nums[i], i);
+        // }
 
-        return false;
+        // return false;
 
         // Idea 2: (Sliding Window + HashSet) use 2 pointers for the beginning and end of acceptable window (abs(i-j)<=k).
         // Add values in HS as iterating acceptable 'window'. If no values are equal -> remove from HS first value and increment both pointer.
 
+        Set<Integer> duplicates = new HashSet<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            // start removing first element of window from HS beginning from k-iteration
+            if(i > k)
+                duplicates.remove(nums[i - k - 1]);
+            if(duplicates.contains(nums[i]))
+                return true;
+            else
+                duplicates.add(nums[i]);
+            // OR:
+            // if(!set.add(nums[i]))
+            //     return true;
+        }
+
+        return false;
     }
 }
