b4b0a5ef5d739bb223d7e272c79bf84f564e4046
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-09-25 13:11:45 +0200

    Time: 16 ms (88.32%), Space: 56.8 MB (36.2%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Idea 1: (HashMap): when iterating through the array remember in HM value and its position.
+        // For each number check if this value is in HM and if abs(i-j) <= k.
+        // If value is not in HM -> add it there. If constraint with k does not work -> update value and its position in HM with current one.
+
+        Map<Integer, Integer> occurrences = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(occurrences.containsKey(nums[i])) {
+                // check k constraint
+                if(i - occurrences.get(nums[i]) <= k)
+                    return true;
+            }
+
+            occurrences.put(nums[i], i);
+        }
+
+        return false;
+
+        // Idea 2: (Sliding Window + HashSet) use 2 pointers for the beginning and end of acceptable window (abs(i-j)<=k).
+        // Add values in HS as iterating acceptable 'window'. If no values are equal -> remove from HS first value and increment both pointer.
+
+    }
+}
