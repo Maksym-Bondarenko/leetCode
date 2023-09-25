79fee6bb7bb69a0e7e2562ea0bc57aaa32670e1d
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-09-25 12:25:00 +0200

    Time: 13 ms (34.27%), Space: 44.1 MB (15.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -71,36 +71,56 @@ class Solution {
 //         // no invalid inputs were found
 //         return true;
         
-        // Idea 2: (same as Idea 1, but) go through matrix only ones, filling up 3 HS
-        // (1 for rows, 1 for columns, 1 for 3x3-squares)
-        // Time: O(n), Space: O(n) -> time 1*n, space 3*n
+        // // Idea 2: (same as Idea 1, but) go through matrix only ones, filling up 3 HS
+        // // (1 for rows, 1 for columns, 1 for 3x3-squares)
+        // // Time: O(n), Space: O(n) -> time 1*n, space 3*n
 
-        HashSet<Character> rows = new HashSet<Character>();
-        HashSet<Character> columns = new HashSet<Character>();
-        HashSet<Character> cube = new HashSet<Character>();
+        // HashSet<Character> rows = new HashSet<Character>();
+        // HashSet<Character> columns = new HashSet<Character>();
+        // HashSet<Character> cube = new HashSet<Character>();
         
-        for(int i = 0; i < 9; i++) {
-            // clear HSs
-            rows.clear();
-            columns.clear();
-            cube.clear();
+        // for(int i = 0; i < 9; i++) {
+        //     // clear HSs
+        //     rows.clear();
+        //     columns.clear();
+        //     cube.clear();
             
+        //     for (int j = 0; j < 9; j++) {
+        //         if(board[i][j] != '.' && !rows.add(board[i][j]))    // [i,j] <=> [row, col]
+        //             return false;
+        //         if(board[j][i] != '.' && !columns.add(board[j][i])) // [j,i] <=> [col, row]
+        //             return false;
+        //         // Use % for horizontal traversal.
+        //         // Because % increments by 1 for each j : 0%3 = 0 , 1%3 = 1, 2%3 = 2.
+        //         // Use / for vertical traversal.
+        //         // Because / increments by 1 after every 3 j: 0/3 = 0; 1/3 = 0; 2/3 =0; 3/3 = 1.
+        //         int RowIndex = 3 * (i / 3);
+        //         int ColIndex = 3 * (i % 3);
+        //         if(board[RowIndex + j/3][ColIndex + j%3] != '.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
+        //             return false;
+        //     }
+        // }
+        
+        // return true;
+
+        // Idea 3: (HashSet with values coded in Strings) collect the set of things we see, encoded as strings. For example:
+        // '4' in row 7 is encoded as "(4)7".
+        // '4' in column 7 is encoded as "7(4)".
+        // '4' in the top-right block is encoded as "0(4)2".
+        // return false if we ever fail to add something because it was already added (i.e., seen before).
+
+        Set seen = new HashSet();
+        for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if(board[i][j] != '.' && !rows.add(board[i][j]))    // [i,j] <=> [row, col]
+                char number = board[i][j];
+                if (number != '.') {
+                    if (!seen.add(number + " in row " + i) ||
+                        !seen.add(number + " in column " + j) ||
+                        !seen.add(number + " in block " + i/3 + "-" + j/3))
                     return false;
-                if(board[j][i] != '.' && !columns.add(board[j][i])) // [j,i] <=> [col, row]
-                    return false;
-                // Use % for horizontal traversal.
-                // Because % increments by 1 for each j : 0%3 = 0 , 1%3 = 1, 2%3 = 2.
-                // Use / for vertical traversal.
-                // Because / increments by 1 after every 3 j: 0/3 = 0; 1/3 = 0; 2/3 =0; 3/3 = 1.
-                int RowIndex = 3 * (i / 3);
-                int ColIndex = 3 * (i % 3);
-                if(board[RowIndex + j/3][ColIndex + j%3] != '.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
-                    return false;
+                }
             }
         }
-        
         return true;
     }
 }
