57109eb16cc5356709eb3dd46709dc5c393e1222
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-01 14:54:29 +0200

    Time: 2 ms (98.64%), Space: 43.5 MB (53.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,51 +1,55 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        /*
-        // with HashMap
+        // Idea 1: (sorting) sort both of strings alphabetically.
+        // Iterate through strings and check if all letters are mathcing (equal)
+        // Time: O(nlogn), Space: O(1)
+        
+        // // Idea 2: (Hash-Map) use HM <key, value> = <character, #_of_occurrences_in_word> (frequency)
+        // // fill HM with letters from s. Iterate through t, decrementing values of HM.
+        // // if no such key in HM OR value = 0 -> return false
+        
+        // // corner-cases
+        // if(s.length() != t.length())
+        //     return false;
+        
+        // Map<Character, Integer> letterCount = new HashMap<>();
+        
+        // // fill-up the HM with letterCount of s
+        // for(char letter : s.toCharArray())
+        //     letterCount.put(letter, letterCount.getOrDefault(letter, 0) + 1);
+        
+        // // check for same letterCount of t
+        // for(char letter : t.toCharArray()) {
+        //     Integer numOfOcurrences = letterCount.get(letter);
+            
+        //     if(numOfOcurrences == null || numOfOcurrences <= 0)
+        //         return false;
+        //     else
+        //         letterCount.put(letter, numOfOcurrences - 1);
+        // }
+        
+        // return true;
 
-        // error-case
+        // Idea 3: (array instead of HM) as Idea 2, use array instead of HM (because we have only lowercase English letters)
+
+        // corner-cases
         if(s.length() != t.length())
             return false;
 
-        // fill-up HashMap with count-letters from s
-        HashMap<Character, Integer> lettersCount = new HashMap<>();
-        for(char ch : s.toCharArray()) {
-            Integer amount = lettersCount.get(ch);
-            lettersCount.put(ch, amount != null ? amount + 1 : 1);
-        }
+        int[] letterCount = new int[26];
 
-        // go through t, eliminating countLertters from HM
-        for(char ch : t.toCharArray()) {
-            Integer currentLetterCount = lettersCount.get(ch);
-            if(currentLetterCount == null || currentLetterCount == 0) {
-                // if no such letter from t in HM (or if count < 0) -> return false
+        // increment frequences of all letters
+        for(char letter : s.toCharArray())
+            letterCount[letter - 'a']++;
+
+        // check frequences
+        for(char letter : t.toCharArray()) {
+            if(letterCount[letter - 'a'] <= 0)
                 return false;
-            } else {
-                // decrese counter in HM
-                lettersCount.put(ch, currentLetterCount - 1);
-            }
+            else
+                letterCount[letter - 'a']--;
         }
 
-        return true;
-        */
-
-        // with array
-        if (s.length() != t.length()) {
-            return false;
-        }
-        
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
-        }
-        
-        for (int i = 0; i < freq.length; i++) {
-            if (freq[i] != 0) {
-                return false;
-            }
-        }
-        
         return true;
     }
 }
