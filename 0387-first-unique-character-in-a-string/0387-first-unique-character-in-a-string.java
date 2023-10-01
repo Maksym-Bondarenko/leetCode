873817fe4539c8442a16cbcce2ca11469c9e1865
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-01 13:34:08 +0200

    Time: 5 ms (90.28%), Space: 44.1 MB (57.79%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -26,25 +26,37 @@ class Solution {
         // // no unique chars
         // return -1;
         
-        // Idea 2: (alternative HM with occurrences) fill the hashMap with occurrences of all letters
-        // return first element where occurrence = 1
+        // // Idea 2: (alternative HM with occurrences) fill the hashMap with occurrences of all letters
+        // // return first element where occurrence = 1
+        // // Runtime: 27ms (36.62%), Memory: 43.72mb (92.16%)
         
-        Map<Character, Integer> numberOfChars = new HashMap<>();
-        Integer occurrences = 0;
+        // Map<Character, Integer> numberOfChars = new HashMap<>();
+        // Integer occurrences = 0;
 
-        // fill the hashMap with occurrences of all letters
+        // // fill the hashMap with occurrences of all letters
+        // for(char i : s.toCharArray())
+        //     numberOfChars.put(i, numberOfChars.getOrDefault(i, 0) + 1);
+        
+        // // return first element with value (occurrences) = 1
+        // for(int i = 0; i < s.length(); i++) {
+        //     occurrences = numberOfChars.get(s.charAt(i));
+        //     if(occurrences == 1)
+        //         return i;
+        // }
+        // return -1;
+        
+        // Idea 3: (array of fixed length for letters) use an array and not HM (better cache-locality, faster, less space-consumption)
+        int[] charCounter = new int[26];
+
+        // each element of array will represent #_of_occurrences of letter under its index
         for(char i : s.toCharArray())
-            numberOfChars.put(i, numberOfChars.getOrDefault(i, 0) + 1);
+            charCounter[i - 'a']++;
         
-        // return first element with value (occurrences) = 1
-        for(int i = 0; i < s.length(); i++) {
-            occurrences = numberOfChars.get(s.charAt(i));
-            if(occurrences == 1)
+        // find first letter with #_of_occurrences = 1
+        for(int i = 0; i < s.length(); i++)
+            if(charCounter[s.charAt(i) - 'a'] == 1)
                 return i;
-        }
+
         return -1;
-        
-        // Idea 3: (array of fixed length for letters)
-        
     }
 }
