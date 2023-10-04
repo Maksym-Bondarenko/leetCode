239f90f5d0dede381560bfd91293c9733dd2e01e
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-04 17:22:42 +0200

    Time: 0 ms (100.00%), Space: 40.1 MB (85.74%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,26 +1,36 @@
 class Solution {
     public int strStr(String haystack, String needle) {
+        // Idea: (two-pointer approach)
+        // one pointer for haystack and one for needle
+        // iterate the word if chars are equal. If reached end of needle -> return true
+        // if did not reach end of needle (letter were unequal)
+        // -> start from beginning of needle and next char (of previously checked start)
+        //    of haystack (<=> haystack-index minus needle-index)
+        
         int hLen = haystack.length();
         int nLen = needle.length();
-        int nIndex = 0;
+        int hIndex = 0; // pointer for haystack
+        int nIndex = 0; // pointer for needle
 
-        for(int i = 0; i < hLen; i++) {
+        for(hIndex = 0; hIndex < hLen; hIndex++) {
             // as long as the characters are equal, increment needleIndex
-            if(haystack.charAt(i) == needle.charAt(nIndex)) {
+            if(haystack.charAt(hIndex) == needle.charAt(nIndex)) {
                 nIndex++;
             }
             else{
-                // start from the next index of previous start index
-                i -= nIndex;
+                // start from the next index of previously started word
+                hIndex -= nIndex;
                 // needle should start from index 0
                 nIndex = 0;
             }
             // check if needleIndex reached needle length
             if(nIndex == nLen){
-                // return the first index
-                return i - nLen + 1;
+                // return the first index <=> endOfWord minus lengthOfWord
+                // (as 'nIndex' = 'nLen')
+                return hIndex - nLen + 1;
             }
         }
+        
         return -1;
     }
 }
