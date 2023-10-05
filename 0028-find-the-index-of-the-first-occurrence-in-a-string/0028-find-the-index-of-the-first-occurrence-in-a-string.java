39b8b9f16da8a1ccec73229bf3ee4ba1b2d81aa1
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-05 10:25:45 +0200

    Time: 0 ms (100.00%), Space: 40.5 MB (34.46%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -14,23 +14,47 @@ class Solution {
 
         for(hIndex = 0; hIndex < hLen; hIndex++) {
             // as long as the characters are equal, increment needleIndex
-            if(haystack.charAt(hIndex) == needle.charAt(nIndex)) {
+            if(haystack.charAt(hIndex) == needle.charAt(nIndex))
                 nIndex++;
-            }
-            else{
+            else {
                 // start from the next index of previously started word
                 hIndex -= nIndex;
                 // needle should start from index 0
                 nIndex = 0;
             }
             // check if needleIndex reached needle length
-            if(nIndex == nLen){
+            if(nIndex == nLen) {
                 // return the first index <=> endOfWord minus lengthOfWord
                 // (as 'nIndex' = 'nLen')
                 return hIndex - nLen + 1;
             }
         }
-        
+        // did not find occurrence of needle in haystack
         return -1;
     }
+    
+//         // Same idea, different coding
+
+//         int haylength = haystack.length();
+//         int needlelength = needle.length();
+
+//         // edge-case (word will not fit in)
+//         if(haylength < needlelength)
+//             return -1;
+
+//         // need to substract lengths, for avoiding out-of-bound in array
+//         for(int i = 0; i <= haystack.length() - needle.length(); i++) {
+//             int j = 0;    // pointer for needle
+
+//             // while words are chars-equal
+//             while(j < needle.length() && haystack.charAt(i + j) == needle.charAt(j))
+//                 j++;
+
+//             // if the whole word is equal
+//             if(j == needle.length())
+//                 return i;
+//         }
+
+//         return -1;
+//    }
 }
