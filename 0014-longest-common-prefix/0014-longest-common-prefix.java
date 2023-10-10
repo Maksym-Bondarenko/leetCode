c595b55c419f7d072ef4c17e5799a0dea91c8901
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-10 16:55:20 +0200

    Time: 1 ms (82.93%), Space: 39.7 MB (98.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -27,29 +27,55 @@ class Solution {
         
 //         return longestPrefix;
         
-        // Idea 2: (alternative, one array-walkthrough only, without words-boundaries)
-        String prefix = "";
-        int charIndex = 0;
+//         // Idea 2: (alternative, one array-walkthrough only, without words-boundaries)
+//         String prefix = "";
+//         int charIndex = 0;
         
-        // since there are strict boundaries for words in 'strs' and length of each word,
-        // we can use this loop (it HAS TO be end-of-word or distinguishing char before overflow)
-        while(charIndex < strs[0].length()) {
-            // since ALL words must have common prefix
-            // -> we can just select first word as starting point
-            char currentChar = strs[0].charAt(charIndex);
+//         // since there are strict boundaries for words in 'strs' and length of each word,
+//         // we can use this loop (it HAS TO be end-of-word or distinguishing char before overflow)
+//         while(charIndex < strs[0].length()) {
+//             // since ALL words must have common prefix
+//             // -> we can just select first word as starting point
+//             char currentChar = strs[0].charAt(charIndex);
             
-            // check all other words on additional char in common prefix
-            for(int i = 1; i < strs.length; i++) {
-                // exit if word has ended or chars are non-equal
-                if(strs[i].length() <= charIndex || strs[i].charAt(charIndex) != currentChar)
-                    return prefix;  // if some word has ended or distinguished char was found
-            }
+//             // check all other words on additional char in common prefix
+//             for(int i = 1; i < strs.length; i++) {
+//                 // exit if word has ended or chars are non-equal
+//                 if(strs[i].length() <= charIndex || strs[i].charAt(charIndex) != currentChar)
+//                     return prefix;  // if some word has ended or distinguished char was found
+//             }
             
-            // add common char to the prefix
-            prefix += currentChar;
-            charIndex++;
+//             // add common char to the prefix
+//             prefix += currentChar;
+//             charIndex++;
+//         }
+        
+//         return prefix;  // if first word is a whole longestCommonPrefix
+        
+        // Idea 3: (sorting lexicographically)
+        // 1. Initialize an empty string ans to store the common prefix.
+        // 2. Sort the input list v lexicographically. This step is necessary because the common prefix should be common to all the strings, so we need to find the common prefix of the first and last string in the sorted list.
+        // 3. Iterate through the characters of the first and last string in the sorted list, stopping at the length of the shorter string.
+        // 4. If the current character of the first string is not equal to the current character of the last string, return the common prefix found so far.
+        // 5. Otherwise, append the current character to the ans string.
+        // 6. Return the ans string containing the longest common prefix.
+        
+        Arrays.sort(strs);
+        String s1 = strs[0];               // first word
+        String s2 = strs[strs.length - 1]; // last word
+        int idx = 0;
+        
+        // first and last sorted words are having different lengths
+        // all words inbetween have same prefix
+        while(idx < s1.length() && idx < s2.length()) {
+            if(s1.charAt(idx) == s2.charAt(idx))
+                idx++;
+            else
+                break;  // stop, chars are non-equivalent
         }
         
-        return prefix;  // if first word is a whole longestCommonPrefix
+        // does not matter from which (first or last) string take a substring
+        return s1.substring(0, idx);
+
     }
 }
