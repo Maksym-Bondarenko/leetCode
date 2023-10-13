e39ab0ed088677ed5223ea4d3f9a417cae4156d0
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-13 16:54:04 +0200

    Time: 0 ms (100%), Space: 40.9 MB (84.5%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,30 +10,42 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // Iterative  
+//         // Idea 1: (Iterative)
+//         // in loop through the LL, reverse at a time one 'arrow' by using 3 pointers:
+//         // 'previous', 'current', 'next'
+        
+//         // corner-cases
 //         if(head == null || head.next == null)
 //             return head;
         
 //         ListNode current = head;
-//         ListNode previous = null;
+//         ListNode previous = null;   // so by first iteration future tail (previous head) points to 0
         
 //         while(current != null) {
-//             ListNode tmp = current.next;
-//             current.next = previous;
-//             previous = current;
+//             ListNode tmp = current.next;    // 'tmp' for next Node
+//             current.next = previous;        // changing direction
+//             previous = current;             // move pointers one step forward for next iteration
 //             current = tmp;
 //         }
         
+//         // return 'previous' as a head (previously tail-element, future 'head'), as 'current' is null
 //         return previous;
         
-        // Recursive
+        // Idea 2: (Recursive)
+        // 
+        
+        // exit-cases from recursion
         if(head == null || head.next == null)
             return head;
         
-        ListNode reversedRest = reverseList(head.next);  // suppose already reversed part
+        // recursive call: suppose rest-part is already reversed
+        ListNode reversedRest = reverseList(head.next);
+        // ! attention to 'next.next' => reverse 'arrow' !
         head.next.next = head;
+        // last element (tail)
         head.next = null;
         
+        // return head (previously tail)
         return reversedRest;
     }
 }
