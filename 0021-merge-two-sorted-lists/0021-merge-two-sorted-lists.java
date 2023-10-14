e8b30faae37715b3919cdc38cf1a59be69a5f1a3
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2023-10-14 12:08:55 +0200

    Time: 0 ms (100%), Space: 41.1 MB (79.49%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,49 +10,57 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Recursive
-        // exit-case
-        if(list1 == null)
-            return list2;
-        else if (list2 == null)
-            return list1;
+        // // Idea 1: (Recursive)
+        // // if some of lists is null -> return another list (it works because lists are sorted)
+        // // else, take a smaller value from lists and append to it result of calling recursive function
+        // // (with input values from both lists)
         
-        // reccurance
-        if(list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
-        }
-
-        // // Iterative
-        // ListNode result = new ListNode(-1);
-        // ListNode current = result;
+        // // exit-cases
+        // if(list1 == null)
+        //     return list2;
+        // else if (list2 == null)
+        //     return list1;
         
-        // while(list1 != null || list2 != null) {
-        //     // add full list if another is ended
-        //     if(list2 == null) {
-        //         current.next = list1;
-        //         current = current.next;
-        //         break;
-        //     } else if(list1 == null) {
-        //         current.next = list2;
-        //         current = current.next;
-        //         break;
-        //     }
-            
-        //     if(list1.val <= list2.val) {
-        //         current.next = new ListNode(list1.val);
-        //         current = current.next;
-        //         list1 = list1.next;
-        //     } else {
-        //         current.next = new ListNode(list2.val);
-        //         current = current.next;
-        //         list2 = list2.next;
-        //     }
+        // // reccurance (we know that both lists are not-null)
+        // if(list1.val <= list2.val) {
+        //     list1.next = mergeTwoLists(list1.next, list2);
+        //     return list1;
+        // } else {
+        //     list2.next = mergeTwoLists(list1, list2.next);
+        //     return list2;
         // }
+
+        // Idea 2: (Iterative)
+        // untill both LL are empty (iterated) create a new node with smaller value
+        // and append it to the result-LL.
+        // If any of LL ended earlier -> append another to the result-LL.
+
+        ListNode result = new ListNode(-1); // dummy-node
+        ListNode current = result;          // for iterations
+
+        // repeat untill both lists are empty        
+        while(list1 != null || list2 != null) {
+            // edge-cases: add full list if another is ended and finish
+            if(list2 == null) {
+                current.next = list1;
+                break;
+            } else if(list1 == null) {
+                current.next = list2;
+                break;
+            }
+            
+            if(list1.val <= list2.val) {
+                current.next = new ListNode(list1.val);
+                list1 = list1.next; // move to new next value
+            } else {
+                current.next = new ListNode(list2.val);
+                list2 = list2.next; // move to new next value
+            }
+
+            // step forward of result-LL
+            current = current.next;
+        }
         
-        // return result.next;
+        return result.next; // skip first dummy-value
     }
 }
