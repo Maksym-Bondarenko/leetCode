835cf2e8aa4ffabfffe2fcdd1e6d34c86c7d7333
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2024-09-17 21:54:56 +0200

    Time: 0 ms (100%), Space: 42.4 MB (24.46%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        // Idea:
+        // Idea (Time: O(N), Space: O(N)):
         // 1. find the biggest element(s) in array
         // 2. iterate through array by adding 'extraCandies' to each current element and checking if it is >= max.element
         // (no corner-cases, according to the task)
@@ -14,6 +14,7 @@ class Solution {
         return kidsWithGreatestNumberOfCandies;
     }
 
+    // helper-function to get max element of given int-array (assuming all elements are positive)
     private int getKidWithMaxCandies(int candies[]) {
         int currentMax = -1;
 
