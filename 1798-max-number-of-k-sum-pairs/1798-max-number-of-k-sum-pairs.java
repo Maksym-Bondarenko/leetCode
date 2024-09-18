da2a323dfe945bf4fae88942b03acaa3a26706c1
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2024-09-18 15:13:57 +0200

    Time: 18 ms (89.55%), Space: 57 MB (69.92%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -2,26 +2,46 @@ class Solution {
     public int maxOperations(int[] nums, int k) {
         // rephrase: how many DISTINCT pares that comes up to k in the array nums
         // Idea 1 <Time: O(N * log N)>: sort array and use two-pointers approach (one from start, second from end, moving to each other) check if sum of array on pointers = k
+        
+        // // (key, value); key = needed value to build a pair; value = amount of keys
+        // Map<Integer, Integer> numsMap = new HashMap();
+        // int operationsCount = 0;
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     // value to build a pair with 'nums[i]'
+        //     int res = k - nums[i];
+
+        //     if(numsMap.containsKey(res)) {
+        //         operationsCount++;
+        //         if(numsMap.get(res) == 1)
+        //             numsMap.remove(res);
+        //         else
+        //             numsMap.put(res, numsMap.get(res) - 1);
+        //     } else
+        //         numsMap.put(nums[i], numsMap.getOrDefault(nums[i], 0) + 1);
+        // }
+
+        // return operationsCount;
+
         // Idea 2 <Time: O(N)>: use HashMap with value of (k-i) for each array-value
 
-        // (key, value); key = needed value to build a pair; value = amount of keys
-        Map<Integer, Integer> numsMap = new HashMap();
-        int operationsCount = 0;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0;
+        int start = 0;
+        int end = n - 1;
 
-        for(int i = 0; i < nums.length; i++) {
-            // value to build a pair with 'nums[i]'
-            int res = k - nums[i];
-
-            if(numsMap.containsKey(res)) {
-                operationsCount++;
-                if(numsMap.get(res) == 1)
-                    numsMap.remove(res);
-                else
-                    numsMap.put(res, numsMap.get(res) - 1);
-            } else
-                numsMap.put(nums[i], numsMap.getOrDefault(nums[i], 0) + 1);
+        while(start < end){
+            if(nums[start] + nums[end] == k) {
+                start++;
+                end--;
+                ans++;
+            } else if(nums[start] + nums[end] > k)
+                end--;
+            else
+                start++;
         }
-
-        return operationsCount;
+        
+        return ans;
     }
 }
