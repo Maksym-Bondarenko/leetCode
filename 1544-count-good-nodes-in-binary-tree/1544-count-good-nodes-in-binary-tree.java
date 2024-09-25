2da877925767165f5d39acd18f097f316b37fba9
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2024-09-25 17:13:43 +0200

    Time: 2 ms (84.09%), Space: 52 MB (86.86%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        // Idea: use DFS for Tree-traversal and keep track on the current largest element
+
+        int currentMax = Integer.MIN_VALUE;
+        int goodNodesCount = 0;
+
+        return dfsTravers(root, currentMax);
+    }
+
+    // help-function to do recursive dfs and calculate amount of good nodes
+    private int dfsTravers(TreeNode root, int currentMax) {
+        // exit from recursion
+        if (root == null)
+            return 0;
+        
+        // recursive calls for children
+        if (root.val >= currentMax) {
+            // node is good, continue with updated (increased) maximum
+            int leftChild = dfsTravers(root.left, root.val);
+            int rightChild = dfsTravers(root.right, root.val);
+
+            return 1 + leftChild + rightChild;
+        } else {
+            // node is not good, continue with old maximum
+            int leftChild = dfsTravers(root.left, currentMax);
+            int rightChild = dfsTravers(root.right, currentMax);
+
+            return leftChild + rightChild;
+        }
+    }
+}
