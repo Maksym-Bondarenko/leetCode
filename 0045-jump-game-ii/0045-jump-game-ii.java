c7b72706f9e04c28c2eaea6f0e8211e6bf5de763
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2024-11-22 14:45:46 +0100

    Time: 1 ms (99.15%), Space: 44.7 MB (93.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
-        // Idea: create an array of "cheapest jumps", representing minimal cost to get to each index
+        /*
+        // Idea 1: create an array of "cheapest jumps", representing minimal cost to get to each index
 
         int[] jumpSteps = new int[nums.length];
         Arrays.fill(jumpSteps, Integer.MAX_VALUE);
@@ -18,5 +19,27 @@ class Solution {
 
         // return (cheapest) price for the last element
         return jumpSteps[nums.length - 1];
+        */
+
+        // Idea 2: algorithm keeps track of the farthest reachable position at each step and updates the number of jumps needed to reach that farthest position
+
+        int steps = 0;
+        int end = 0;
+        int farthest = 0;
+
+        // Implicit BFS
+        for (int i = 0; i < nums.length - 1; ++i) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (farthest >= nums.length - 1) {
+                ++steps;
+                break;
+            }
+            if (i == end) {   // Visited all the items on the current level
+                ++steps;          // Increment the level
+                end = farthest; // Make the queue size for the next level
+            }
+        }
+
+        return steps;
     }
 }
