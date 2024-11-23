5f33ef45637f9c8d6c20f1d0f280af4db194c6d1
Maksym Bondarenko <45164667+Maksym-Bondarenko@users.noreply.github.com>
2024-11-23 20:51:11 +0100

    Time: 87 ms (8.27%), Space: 62.9 MB (33.23%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        // Time: O(n)
+        
+        int[] answer = new int[temperatures.length];   // initially all values are 0
+        Stack<Integer> tempIndices = new Stack<>();    // to save 'falling' sequence of temperatures
+
+        for (int i = 0; i < temperatures.length; i++) {
+            // if today temperature is above any day of before (that are still 0)
+            // add days-differenz to result-array and remove value from stack
+            // important: repear for possible multiple values!
+            while (!tempIndices.isEmpty() &&
+                    temperatures[i] > temperatures[tempIndices.peek()]) {
+                // set values for some previous day (= saved index in stack)
+                // how many days <=> indices, not array-values!
+                answer[tempIndices.peek()] = i - tempIndices.peek();
+                tempIndices.pop();
+            }
+            
+            tempIndices.push(i);
+        }
+
+        return answer;
+    }
+}
